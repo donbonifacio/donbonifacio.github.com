c885a77a1c0023f3c9c62e695083dfce60d6d6ff
Pedro Santos <donbonifacio@gmail.com>
2012-10-19 14:21:26 -0400

    Started data

---
+++ script/data.js
@@ -0,0 +1,10 @@
+window.data = {
+    projects: [
+            {
+                name: "Orion's Belt",
+                description: "",
+                year: 2006,
+                tags: []
+            }
+        ]
+};
